35d3a506bfb104728c45c87d52a2a19432223486
Seweryn Kozik <sewkozik@gmail.com>
2023-10-15 00:13:32 +0200

    Add auto ping feature

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,6 +1,7 @@
 import { Client, ClientOptions, GatewayIntentBits } from "discord.js";
 import ready from "./listeners/ready";
 import interactionCreate from "./listeners/interactionCreate";
+import voiceStateUpdate from "./listeners/voiceStateUpdate";
 import 'dotenv/config'
 
 const token = process.env.TOKEN;
@@ -19,6 +20,7 @@ const client = new Client({
 
 ready(client);
 interactionCreate(client);
+voiceStateUpdate(client)
 
 client.login(token);
 
